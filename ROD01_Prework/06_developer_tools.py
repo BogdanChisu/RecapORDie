2b31549436e97ea27856c950ae98bab5f0bdcdf0
Bogdan Chisu <bchisu@gmail.com>
2024-03-14 20:52:53 +0200

    added 2 sections

---
+++ ROD01_Prework/06_developer_tools.py
@@ -0,0 +1,48 @@
+"""
+Developer Tools
+================
+
+1. Compiler or Interpreter
+2. Debugger
+3. Monitoring (Open JDK)
+4. Documentation
+
+Build Tools
+-----------
+Maven and Gradle for Java
+- Make the compilation process simpler
+- Automate library download
+
+Editor or IDE
+-------------
+Integrated Development Environments
+Lightweight editors require plugins to be installed to facilitate code
+writing in one or another language.
+- Visual Studio Code
+- Atom
+- Notepad++
+- Intellij IDEA
+- Eclipse
+
+VCS Version Control System
+--------------------------
+Tracks the changes in the code
+Source code "version" notation
+Versions comparison
+Undoing changes
+- git
+- GitHub
+- GitLab
+
+Browser
+-------
+Searching for solution is paramount for every coder
+- Google, DuckDuckGo
+- StackOverflow
+
+Communicators
+-------------
+- Slack
+- Discord
+- Microsoft Teams
+"""
---
+++ ROD01_Prework/07_how_the_internet_works.py
@@ -0,0 +1,58 @@
+"""
+INTERNET
+========
+
+IP - Internet Protocol
+----------------------
+Every computer connected to the internet has its own unique IP address.
+
+IPv4 address value has 32 bits.
+e.g. - 172.217.21.206
+
+Alternative - IPv6 values are stored on 128 bit addresses which reduce the
+risk of running out of supply - sadly it is not so widely supported
+e.g. - 2a00:1450:4001:818::200e
+
+TCP - Transmission Control Protocol
+-----------------------------------
+More popular than UDP it is used for visiting internet sites.
+- Bilateral communication - for every message sent through a connection,
+  a response may be received via the same connection.
+- confirmation of message receipt for every message sent through such a
+  connection
+- receive and order of messages are guaranteed
+
+UDP - User Datagram Protocol
+----------------------------
+- Messages are sent one-way and the computer instantly forgets about them
+- There is no guarantee that the messages will be received in the order in
+  which they were sent or at all
+- Offers less delay than TCP
+- Used in multiplayer games
+
+DNS - Domain Name System
+------------------------
+When you type an address like google.com or sdacademy.com, you do not know
+the IP address.
+Your computer will know the address of a DNS server.
+The information provided by the server corresponds but is not limited to the IP
+address corresponding to the domain.
+
+HTTP - Hypertext transfer protocol
+-----------------------------------
+After the TCP connection to the domain you want to visit has been realized,
+the way will be visiting the website is defined by the HTTP protocol.
+This is a protocol that acts on a request-and-response basis.
+Your browser sends a request to HTTP server which responds to it
+
+HTTP requests (Methods)
+-----------------------
+GET - method used for requesting the contents of a website
+
+e.g.
+Simple request of HTTP website google.com/mail (sent by TCP)
+GET/mail HTTP/1.1 => method name, http protocol version
+Host:google.com   => host (receiver of the request)
+/mail => path to the content we request
+The server will send us the response in the form of an HTML document
+"""
