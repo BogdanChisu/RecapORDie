65dc00b4b7e65408a224c42c2516402cbba75f4f
Bogdan Chisu <bchisu@gmail.com>
2024-04-27 06:33:50 +0300

    added one file

--- ROD01_Prework/14_environment_variables.py
+++ ROD01_Prework/14_environment_variables.py
@@ -3,8 +3,8 @@ Environment Variables
 =====================
 
 Operating systems often contain a lot of application programs that require
-different software to run, e.g. a programming language or an additional library.
-System variables have been created to speed up communication between software
-and to speed up the way a user runs software. A variable in its definition is a
-certain object in which we can store a value. 
+different software to run, e.g. a programming language or an additional
+library. System variables have been created to speed up communication
+between software and to speed up the way a user runs software. A variable in
+its definition is a certain object in which we can store a value.
 """
